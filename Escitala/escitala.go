d63a821821c29e69828d2757dfe2a6b5353a6815
Iker Pacheco Herrero <iker.pacheco@bimaxpro.net>
2026-02-22 23:13:58 +0100

    UWU

---
+++ Escitala/escitala.go
@@ -0,0 +1,21 @@
+/*
+
+CLAVE = NUMERO DE CARAS PRISMA
+LONGITUD = 6 CARAS (LA PIDO TAMBIEN)
+
+VOY A PEDIR ANCHO Y LARGO DE LA MATRIZ
+
+LA MATRIZ QUE HE DE MONTAR, MONTO POR COLUMNAS Y LEO POR FILAS
+
+[H,M,O,A]
+[O,U,E,Ñ]
+[L,N,S,A]
+[A,D,P, ]
+
+HMOAOUEÑLNSADPA_
+
+make 2 dimensiones make([][]rune, tamaño)
+
+recorro la matriz y voy metiendo los valores
+
+*/
